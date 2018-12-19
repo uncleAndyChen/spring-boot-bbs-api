233b3da4e73355763c54c33a83ac093c2443a66e
AndyChen <andy@lovesofttech.com>
2018-12-19 23:22:10 +0800

    部分代码重构和优化。另外是与前端项目对接，完成情况如下： 1. 完成帖子详情页面: 帖子详情获取、帖子评论列表获取 2. 完成登录功能 3. 待测试：创建评论

---
+++ bbs-api-biz/bbs-api-biz-model/src/main/java/bbs/api/biz/model/response/CreateCommentResponse.java
@@ -0,0 +1,49 @@
+package bbs.api.biz.model.response;
+
+public class CreateCommentResponse {
+    private int id;
+    private int postId;
+    private int userId;
+    private String content;
+    private String updatedAt;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
